02ee6f550407e15cef375d12eaac1d5e5862454d
hamidreza babazamani <hamidreza102030@gmail.com>
2026-09-25 11:07:08 +0330

    feat: add HomePage, LoginPage, ProfilePage with routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,16 @@
-import Sidebar from './components/Sidebar'
-import ChatContainer from './components/ChatContainer'
-import RightSidebar from './components/RightSidebar'
+import { Route, Routes } from 'react-router-dom'
+import HomePage from './pages/HomePage'
+import LoginPage from './pages/LoginPage'
+import ProfilePage from './pages/ProfilePage'
 
-function App() {
+const App = () => {
   return (
-    <div className="min-h-screen bg-cover bg-center bg-[url('./assets/bgImage.svg')]">
-      <div className="min-h-screen w-full bg-black/50 backdrop-blur-lg flex items-center justify-center">
-        <div className="w-full max-w-6xl h-[90vh] rounded-2xl overflow-hidden shadow-2xl grid grid-cols-[1fr_2fr_1fr]">
-          <Sidebar />
-          <ChatContainer />
-          <RightSidebar />
-        </div>
-      </div>
+    <div>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+      </Routes>
     </div>
   )
 }
